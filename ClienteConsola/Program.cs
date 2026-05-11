252869c36914f41baeb6ba6be5483ac7984db1fd
Ignacio Sanchez <igna.sanchez.1999@gmail.com>
2026-05-10 23:29:17 -0300

    Subiendo a github la demo

---
+++ ClienteConsola/Program.cs
@@ -0,0 +1,38 @@
+﻿using Grpc.Net.Client;
+using SistemaHardware;
+using System.Net.Http;
+
+var httpHandler = new HttpClientHandler();
+httpHandler.ServerCertificateCustomValidationCallback =
+    HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7233", new GrpcChannelOptions { HttpHandler = httpHandler });
+var client = new Inventario.InventarioClient(channel);
+
+Console.WriteLine("--- CLIENTE INICIADO ---");
+Console.WriteLine("Escribí 'salir' para terminar el programa.");
+
+while (true)
+{
+    try
+    {
+        Console.Write("\nIngrese el nombre del componente (ej: RTX 5090): ");
+        string producto = Console.ReadLine() ?? "";
+
+        if (producto.ToLower() == "salir") break;
+
+        var reply = await client.ConsultarStockAsync(new StockRequest { NombreProducto = producto });
+
+        Console.WriteLine($"SERVIDOR DICE: {reply.Mensaje}");
+        Console.WriteLine($"CANTIDAD EN STOCK: {reply.Cantidad}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"\nERROR: {ex.Message}");
+      
+        break;
+    }
+}
+
+Console.WriteLine("\nConexión finalizada. Presione cualquier tecla para salir...");
+Console.ReadKey();
---
+++ ServidorHardware/Services/HardwareService.cs
@@ -0,0 +1,18 @@
+﻿using Grpc.Core;
+using SistemaHardware; 
+
+namespace ServidorHardware.Services;
+
+public class HardwareService : Inventario.InventarioBase
+{
+    public override Task<StockReply> ConsultarStock(StockRequest request, ServerCallContext context)
+    {
+        int stock = (request.NombreProducto.ToUpper() == "RTX 5090") ? 5 : 0;
+
+        return Task.FromResult(new StockReply
+        {
+            Cantidad = stock,
+            Mensaje = stock > 0 ? "Producto encontrado" : "Sin stock"
+        });
+    }
+}
